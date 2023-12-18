12f4c063719b2324dfa27e9750bf14e815e27a28
Frederic Bachmann <frederic.bachmann@gmail.com>
2023-12-18 09:31:10 +0100

    production configuration

--- app/user-pads/pad/[padId]/page.tsx
+++ app/user-pads/pad/[padId]/page.tsx
@@ -2,10 +2,16 @@ import prisma from '@/app/db'
 import { notFound, redirect } from 'next/navigation'
 import { auth } from '@/utils/auth'
 import { PadAppBar } from './app-bar'
+import { cookies } from 'next/headers'
 
 export default async function page({ params }: { params: { padId: string } }) {
   const session = await auth()
   if (!session) redirect('/api/auth/signin')
+
+  const cookie =
+    cookies().get('next-auth.session-token') ||
+    cookies().get('__Secure-next-auth.session-token')
+  const token = cookie!.value
 
   const pad = await prisma.pad.findUnique({
     where: {
@@ -45,7 +51,7 @@ export default async function page({ params }: { params: { padId: string } }) {
       />
       <iframe
         name='embed_readwrite'
-        src={`${process.env.ETHERPAD_EXTERNAL_URL}/p/${params.padId}`}
+        src={`${process.env.ETHERPAD_EXTERNAL_URL}/p/${params.padId}?token=${token}`}
         className='w-full flex-1'
       />
     </div>
